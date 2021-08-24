e7afa912fae5b9e01b05cf9b149240dc952f43b9
cy322666 <cy322666@gmail.com>
2021-08-24 22:40:37 +0300

    [dev][local] hook build + add migrate

--- app/Http/Controllers/ChangeController.php
+++ app/Http/Controllers/ChangeController.php
@@ -2,13 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Change;
+use Illuminate\Support\Facades\Log;
 use Laravel\Lumen\Http\Request;
+use Ufee\Amo\Models\Lead;
 
 class ChangeController extends Controller
 {
     public function hook(Request $request)
     {
+        Log::info(__METHOD__, $request->toArray());
 
+        if($request->toArray()['update'][0]['pipeline_id'] == 4582795) {
+
+
+            $lead_id = $request->toArray()['update'][0]['lead_id'];
+
+            $custom_fields = $request->toArray()['update'][0]['custom_fields'];
+
+            if(count($custom_fields) > 0) {
+
+                $lead = Change::where('lead_id', $lead_id)
+                    ->where('status', 'OK')
+                    ->first();
+
+                if(!$lead) {
+
+                    foreach ($custom_fields as $custom_field) {
+
+                        if($custom_field['id'] == 760347 && $custom_field['id']['values']['value'] == 'Да') {
+
+                            Lead::create([
+                                'lead_id' => $lead_id,
+                                'value' => $custom_field['id']['values']['value'],
+                            ]);
+                        }
+                    }
+                } else
+                    Log::warning('Дубль отработанного хука : '. $lead->lead_id);
+            } else
+                Log::warning('Нет изменений в полях : '. $lead_id, $custom_fields);
+        } else
+            Log::warning('Не нужная воронка : '. $request->toArray()['update'][0]['pipeline_id']);
     }
 
     public function cron()
---
+++ database/migrations/2021_08_24_222222_create_changes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChangesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('changes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('lead_id')->nullable();
+            $table->string('value')->nullable();
+            $table->integer('company_id')->nullable();
+            $table->string('status')->default('Первично добавлена');
+
+            $table->index('lead_id');
+            $table->index('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('changes');
+    }
+}
