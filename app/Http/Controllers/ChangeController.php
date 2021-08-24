0ada9fcb9f10ff72523394de90abcbc8188410ab
cy322666 <cy322666@gmail.com>
2021-08-24 22:06:48 +0300

    [init] add ChangeController.php

---
+++ app/Http/Controllers/ChangeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Http\Request;
+
+class ChangeController extends Controller
+{
+    public function hook(Request $request)
+    {
+
+    }
+
+    public function cron()
+    {
+
+    }
+}
